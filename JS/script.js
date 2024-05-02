7990d3a99755f85b7f926b7fb46aae8cb10f1d87
polyndesign <polyng1@tcnj.edu>
2024-05-02 13:36:42 -0400

    JQuerry Help

--- JS/script.js
+++ JS/script.js
@@ -6,3 +6,22 @@ if (jQuery) {
 else {
     console.log("no jquery")
 }
+
+
+// FOR MOBILE MENU DELETE IF NOT WORKING//
+let mobileMenuOpen = false;
+
+$("#mobile-menu-button").click(function(){
+    //if mobile menu is closed , open it
+   if(mobileMenuOpen == false) {
+    $("#main-menu").removeClass("navigation");
+    mobileMenuOpen = true;  
+    $("#mobile-menu-icon").attr("src","assets/phonemenu/hamburgerbutton.svg");
+   }
+//    if mobile menu is open, close it
+   else {
+    $("#main-menu").addClass("navigation");
+    mobileMenuOpen = false; 
+    $("#mobile-menu-icon").attr("src","assets/phonemenu/hamburgerbutton.svg");
+}
+});
