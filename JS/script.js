cd05f311fb19ac09303183f6e515ce0c9b5ee7d1
polyndesign <polyng1@tcnj.edu>
2024-05-05 19:19:57 -0400

    Updated JQuerry, CSS, and HTML

--- JS/script.js
+++ JS/script.js
@@ -8,19 +8,22 @@ else {
 }
 
 
-// FOR MOBILE MENU DELETE IF NOT WORKING//
+// FOR MOBILE MENU//
 let mobileMenuOpen = false;
 
 $("#mobile-menu-button").click(function(){
     //if mobile menu is closed , open it
    if(mobileMenuOpen == false) {
-    $("#main-menu").removeClass("navigation");
+
+    $("#nav-items").addClass("display-flex");
+    $("#nav-items").removeClassClass("display-none-mobile");
     mobileMenuOpen = true;  
     $("#mobile-menu-icon").attr("src","assets/phonemenu/hamburgerbutton.svg");
    }
 //    if mobile menu is open, close it
    else {
-    $("#main-menu").addClass("navigation");
+    $("#nav-items").removeClass("display-flex");
+    $("#nav-items").addClass("display-none-mobile");
     mobileMenuOpen = false; 
     $("#mobile-menu-icon").attr("src","assets/phonemenu/hamburgerbutton.svg");
 }
